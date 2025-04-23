aefdc582386a478a0469fa6311c104e1b14ceeda
sar-bc@mail.ru <sar-bc@mail.ru>
2025-04-23 23:58:00 +0400

    up

--- app/vik/settings.py
+++ app/vik/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hxj0^hl8a&i)g_jgpxz!si((-fyxtf=tgxsui#7hai#hiir$#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
