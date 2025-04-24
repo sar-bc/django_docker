e318a4af38c49a2cbd4d5741639c79b989231322
sar-bc@mail.ru <sar-bc@mail.ru>
2025-04-24 23:41:48 +0400

    up

--- app/vik/settings.py
+++ app/vik/settings.py
@@ -128,11 +128,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = '/app/static'
+STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = '/app/static'
 STATICFILES_DIRS = []
-MEDIA_ROOT = '/app/media'
-# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
