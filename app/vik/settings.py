ce2dfa1e3f89fcbfe0af8b2e05edbe0067dbaa79
Виктор <sar-bc@mail.ru>
2025-04-26 01:35:41 +0400

    up

--- app/vik/settings.py
+++ app/vik/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hxj0^hl8a&i)g_jgpxz!si((-fyxtf=tgxsui#7hai#hiir$#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django.sar-bc.ru',
